acf3d771c5d7f308220dac4cf55cd8ea1bd0f211
Kihwan Hong <airaider@naver.com>
2024-12-07 21:05:59 +0900

    Time: 0 ms (100%), Space: 17.3 MB (76.11%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
@@ -1,10 +1,10 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        seen = set()
+        numbers = set(arr)
+
+        if arr.count(0) > 1:
+            return True
         
-        for num in arr:
-            if 2 * num in seen or (num % 2 == 0 and num // 2 in seen):
-                return True
-            seen.add(num)
-            
+        for i in arr:
+            if i != 0 and i*2 in numbers: return True
         return False
